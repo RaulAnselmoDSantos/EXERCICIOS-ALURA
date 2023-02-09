f6dc0d8a244654f3aeacfa6e73ea433cd40c25ea
Raul Anselmo D Santos <raul.trabalho5511@gmail.com>
2023-02-09 15:44:35 -0300

    teste valores

--- Exercicios-Alura/java/TestaValores.java
+++ Exercicios-Alura/java/TestaValores.java
@@ -3,6 +3,7 @@ public class TestaValores {
     public static void main(String[] args) {
         int primeiro = 5; 
         int segundo = 7; 
+        segundo = primeiro; 
 
         System.out.println(segundo);
     }
