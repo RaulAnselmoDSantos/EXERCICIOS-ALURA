c62acab57f1b9755518a6ecb49c4848986bbca36
Raul Anselmo D Santos <raul.trabalho5511@gmail.com>
2023-04-11 16:44:23 -0300

    add function "subtrai" and "soma"

--- Exercicios-Alura/java/JavaScript/projeto-js-siteRobos/js/main.js
+++ Exercicios-Alura/java/JavaScript/projeto-js-siteRobos/js/main.js
@@ -2,6 +2,18 @@ const subtrair = document.querySelector('#subtrair')
 const soma = document.querySelector('#somar')
 const braco = document.querySelector('#braco')
 
-soma.addEventListener("click", (evento) => {
-    braco.value += 1
-})
+
+
+soma.addEventListener("click", () => {manipuladados("somar")})
+
+subtrair.addEventListener("click", () => {manipuladados("subtrair")})
+
+
+function manipuladados(operacao) {
+    if(operacao === "subtrair") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
+}
+
